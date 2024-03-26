772910d0500f91508103f6980102153afa992120
Daniel Floresca <dfloresca@gmail.com>
2024-03-25 21:23:41 -0700

    feat: created hero component and imported to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
 
 const App = () => {
   return (
     <>
     <Navbar />
+    <Hero />
       
-      <section className="bg-indigo-700 py-20 mb-4">
-        <div
-          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-        >
-          <div className="text-center">
-            <h1
-              className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-            >
-              Become a React Dev
-            </h1>
-            <p className="my-4 text-xl text-white">
-              Find the React job that fits your skills and needs
-            </p>
-          </div>
-        </div>
-      </section>
-
-
       <section className="py-4">
         <div className="container-xl lg:container m-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
